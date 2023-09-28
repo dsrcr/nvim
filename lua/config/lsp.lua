10e2f8ded13293c91862d77037ac9eb2d45b2556
phajw <dscr@duck.com>
2023-09-28 05:09:11 -0400

    Fix mason lsp

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,16 +1,27 @@
-local lsp = require('lsp-zero')
+-- local lsp = require('lsp-zero')
+
+local lsp_zero = require('lsp-zero')
 local cmp = require('cmp')
 require("luasnip.loaders.from_vscode").lazy_load()
 
-lsp.preset('recommended')
-lsp.setup()
+lsp_zero.preset('recommended')
+lsp_zero.setup()
+lsp_zero.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
 
-lsp.ensure_installed({
-    'tsserver',
-    'eslint',
-    'sumneko_lua',
-    'rust_analyzer',
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  -- Replace the language servers listed here 
+  -- with the ones you want to install
+  ensure_installed = {'tsserver', 'rust_analyzer', 'eslint'},
+  handlers = {
+    lsp_zero.default_setup,
+  },
 })
+
 
 
 require('tabnine').setup({
@@ -23,45 +34,45 @@ require('tabnine').setup({
 })
 
 cmp.setup({
-    snippet = {
-        expand = function(args)
-            require('luasnip').lsp_expand(args.body)
-        end,
-    },
-    window = {
-    },
- mapping = cmp.mapping.preset.insert({
-      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    }),
-    sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
-    }, {
-        { name = 'buffer' },
-    })
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  window = {
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  }, {
+    { name = 'buffer' },
+  })
 })
 
 function PrintDiagnostics(opts, bufnr, line_nr, client_id)
-    bufnr = bufnr or 0
-    line_nr = line_nr or (vim.api.nvim_win_get_cursor(0)[1] - 1)
-    opts = opts or { ['lnum'] = line_nr }
+  bufnr = bufnr or 0
+  line_nr = line_nr or (vim.api.nvim_win_get_cursor(0)[1] - 1)
+  opts = opts or { ['lnum'] = line_nr }
 
-    local line_diagnostics = vim.diagnostic.get(bufnr, opts)
-    if vim.tbl_isempty(line_diagnostics) then return end
+  local line_diagnostics = vim.diagnostic.get(bufnr, opts)
+  if vim.tbl_isempty(line_diagnostics) then return end
 
-    local diagnostic_message = ""
-    for i, diagnostic in ipairs(line_diagnostics) do
-        diagnostic_message = diagnostic_message .. string.format("%d: %s", i, diagnostic.message or "")
-        print(diagnostic_message)
-        if i ~= #line_diagnostics then
-            diagnostic_message = diagnostic_message .. "\n"
-        end
+  local diagnostic_message = ""
+  for i, diagnostic in ipairs(line_diagnostics) do
+    diagnostic_message = diagnostic_message .. string.format("%d: %s", i, diagnostic.message or "")
+    print(diagnostic_message)
+    if i ~= #line_diagnostics then
+      diagnostic_message = diagnostic_message .. "\n"
     end
-    vim.api.nvim_echo({ { diagnostic_message, "Normal" } }, false, {})
+  end
+  vim.api.nvim_echo({ { diagnostic_message, "Normal" } }, false, {})
 end
 
 vim.cmd [[ autocmd! CursorHold * lua PrintDiagnostics() ]]
